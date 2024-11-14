4546f7c3e75fbfa1b3acd04f71c7c7e205f9602a
witrellesp <130034004+witrellesp@users.noreply.github.com>
2024-11-14 10:43:52 +0100

    update(challenge2)
    Défis page 2 DONE
    A améliorer

--- scape-game/src/views/Challenge2.vue
+++ scape-game/src/views/Challenge2.vue
@@ -1,3 +1,22 @@
 <template>
   <h1>Challenge 2</h1>
+  <form action="">
+    <label for="colorBackground">Quelle est la couleur de fond?</label><br />
+    <input type="text" id="inputColor" v-model="inputColor" />
+    <button v-if="inputColor == colorBackground">
+      <RouterLink to="/challenge2">Next challenge</RouterLink>
+    </button>
+  </form>
 </template>
+<style>
+body {
+  /*Color de fond*/
+  background-color: #001780;
+}
+</style>
+<script setup>
+import { ref } from 'vue'
+const colorBackground = '#001780'
+
+const inputColor = ref('')
+</script>
